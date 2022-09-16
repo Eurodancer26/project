f585963095f6f92a0f0db8042e38e0b0136af0fd
Timur <murushkin.timur@gmail.com>
2022-09-17 00:19:14 +0300

    js file mod

--- js/script.js
+++ js/script.js
@@ -39,7 +39,17 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -49,18 +59,23 @@ const personalMovieDB = {
     privat: false
 };
 
-for(let i = 0; i < 2; i++){
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = +prompt('На сколько оцените его?', '');
 
-    if(a != null && b != null && a != '' && b != '' && a.length < 50 && b < +11){
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    }else {
-        console.log('error');
-        i--;
-    }         
+function ememberMyFilms() {
+        for(let i = 0; i < 2; i++){
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = +prompt('На сколько оцените его?', '');
+
+        if(a != null && b != null && a != '' && b != '' && a.length < 50 && b < +11){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        }else {
+            console.log('error');
+            i--;
+        }         
+    }
 }
+
+ememberMyFilms();
 
 if (personalMovieDB.count < 10) {
     alert('Просмотрено довольно мало фильмов');
