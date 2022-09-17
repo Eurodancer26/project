9e162d00355fd8e957945e743456d03957ea212c
Timur <murushkin.timur@gmail.com>
2022-09-17 08:27:14 +0300

    JS file Mod

--- js/script.js
+++ js/script.js
@@ -1,42 +1,3 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
-
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-
 'use strict';
 
 let numberOfFilms;
@@ -49,7 +10,7 @@ function start(){
     }
 }
 
-start();
+//start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -75,16 +36,42 @@ function ememberMyFilms() {
     }
 }
 
-ememberMyFilms();
+//ememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('Вы классический зритель');
-}else if (personalMovieDB.count >= 30) {
-    alert('Вы киноман');
-}else {
-    alert('ERROR');
+
+
+function detectPersonalLevel() {
+        if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    }else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман');
+    }else {
+        alert('ERROR');
+    }
 }
 
-console.log(personalMovieDB);
+//detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if(!hidden){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for(let i = 1; i <=3; i++){
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        if(genre != null && genre != '' && genre.length < 50 && isNaN(genre)){
+            personalMovieDB.genres[i - 1] = genre;
+        }else {
+            console.log('error');
+            i--;
+        } 
+    }
+} 
+
+writeYourGenres();
